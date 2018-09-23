a8062a8fc857deb6fb4d885983f855ad58b60bca
williamblair <blairw@sunyit.edu>
2018-09-23 16:24:22 -0400

    should work now

--- app.py
+++ app.py
@@ -26,145 +26,146 @@ randomNames = [
     'sonny'
 ]
 
-# whether or not dadbot is disabled
-isInTimeout = False
+# use a file b/c I think this app.py runs only
+# when a message is recieved, so a global variable
+# won't save anything
+def changeTimeout(timeoutBool):
+    if (timeoutBool):
+        os.system("echo 1 > isTimeout.txt")
+    else:
+        if os.path.exists("isTimeout.txt"):
+            os.system("rm isTimeout.txt")
 
 # called whenever the bot recieves a POST request
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     
-    #global isInTimeout
-    #print('Is in timeout: ' + str(isInTimeout))
-    
     # we don't want to reply to ourselves!
+    #if data['name'] != 'dad bot tester':
     if data['name'] != 'Dad Bot':
         
         userText = data['text']
         
-        global isInTimeout
-        
         # timeout feature
         if userText.upper() == 'SHUT UP DAD':
-            #global isInTimeout
-            isInTimeout = True
-            send_message('Ok sport... :(', False)
+            changeTimeout(True)
+            send_message('Ok sport... :(')
             return "ok", 200
         
         elif userText.upper() == 'COME BACK DAD' or userText.upper() == 'DAD COME BACK':
-            #global isInTimeout
-            isInTimeout = False
+            changeTimeout(False)
             greetStr = random.choice(randomGreetings)
             nameStr = random.choice(randomNames)
             msg = '{}, {}'.format(greetStr, nameStr)
-            send_message(msg, False)
+            send_message(msg)
             return "ok", 200
         
-        # dad commands perhaps
-        if userText.upper().startswith('DAD '):
-            if userText.split(' ')[1].upper() == 'JOKE':
-                send_dadjoke(isInTimeout)
-            elif userText.split(' ')[1].upper() == 'FORTUNE':
-                send_fortune(isInTimeout)
-        
-        # contains i'm
-        elif ' I\'m ' in userText:
-            send_message('Hi, {}, I\'m Dad!'.format(userText.split(' I\'m ')[1]), isInTimeout)
-        
-        elif ' I\’m ' in userText:
-            send_message('Hi, {}, I\'m Dad!'.format(userText.split(' I\’m ')[1]), isInTimeout)
-        
-        elif ' i\'m ' in userText:
-            send_message('Hi, {}, I\'m Dad!'.format(userText.split(' i\'m ')[1]), isInTimeout)
-        
-        elif ' Im ' in userText:
-            send_message('Hi, {}, I\'m Dad!'.format(userText.split(' Im ')[1]), isInTimeout)
-        
-        elif ' im ' in userText:
-            send_message('Hi, {}, I\'m Dad!'.format(userText.split(' im ')[1]), isInTimeout)
+        print('Dad environ:', os.path.exists("isTimeout.txt"))
+        if not os.path.exists("isTimeout.txt"):
+            # dad commands perhaps
+            if userText.upper().startswith('DAD '):
+                if userText.split(' ')[1].upper() == 'JOKE':
+                    send_dadjoke()
+                elif userText.split(' ')[1].upper() == 'FORTUNE':
+                    send_fortune()
             
-        # starts with im 
-        elif userText.startswith('I\'m'):
-            send_message('Hi, {}, I\'m Dad!'.format(userText.replace('I\'m', '')), isInTimeout)
+            # contains i'm
+            elif ' I\'m ' in userText:
+                send_message('Hi, {}, I\'m Dad!'.format(userText.split(' I\'m ')[1]))
             
-        elif userText.startswith('I’m'):
-            send_message('Hi, {}, I\'m Dad!'.format(userText.replace('I’m', '')), isInTimeout)
-        
-        elif userText.startswith('i\'m'):
-            send_message('Hi, {}, I\'m Dad!'.format(userText.replace('i\'m', '')), isInTimeout)
-        
-        elif userText.startswith('im '):
-            send_message('Hi, {}, I\'m Dad!'.format(userText.replace('im ', '')), isInTimeout)
-        
-        elif userText.startswith('Im '):
-            send_message('Hi, {}, I\'m Dad!'.format(userText.replace('Im ', '')), isInTimeout)
-        
-        elif userText.startswith('IM '):
-            send_message('Hi, {}, I\'m Dad!'.format(userText.replace('IM ', '')), isInTimeout)
+            elif ' I\’m ' in userText:
+                send_message('Hi, {}, I\'m Dad!'.format(userText.split(' I\’m ')[1]))
             
-        elif 'HI DAD' in userText.upper():
-            greetStr = random.choice(randomGreetings)
-            nameStr = random.choice(randomNames)
-            msg = '{}, {}'.format(greetStr, nameStr)
-            send_message(msg, isInTimeout)
+            elif ' i\'m ' in userText:
+                send_message('Hi, {}, I\'m Dad!'.format(userText.split(' i\'m ')[1]))
+            
+            elif ' Im ' in userText:
+                send_message('Hi, {}, I\'m Dad!'.format(userText.split(' Im ')[1]))
+            
+            elif ' im ' in userText:
+                send_message('Hi, {}, I\'m Dad!'.format(userText.split(' im ')[1]))
+                
+            # starts with im 
+            elif userText.startswith('I\'m'):
+                send_message('Hi, {}, I\'m Dad!'.format(userText.replace('I\'m', '')))
+                
+            elif userText.startswith('I’m'):
+                send_message('Hi, {}, I\'m Dad!'.format(userText.replace('I’m', '')))
+            
+            elif userText.startswith('i\'m'):
+                send_message('Hi, {}, I\'m Dad!'.format(userText.replace('i\'m', '')))
+            
+            elif userText.startswith('im '):
+                send_message('Hi, {}, I\'m Dad!'.format(userText.replace('im ', '')))
+            
+            elif userText.startswith('Im '):
+                send_message('Hi, {}, I\'m Dad!'.format(userText.replace('Im ', '')))
+            
+            elif userText.startswith('IM '):
+                send_message('Hi, {}, I\'m Dad!'.format(userText.replace('IM ', '')))
+                
+            elif 'HI DAD' in userText.upper():
+                greetStr = random.choice(randomGreetings)
+                nameStr = random.choice(randomNames)
+                msg = '{}, {}'.format(greetStr, nameStr)
+                send_message(msg)
         
     return "ok", 200
 
-def send_message(msg, isTimeout):
-
-    if not isTimeout:
-        url = 'https://api.groupme.com/v3/bots/post'
+def send_message(msg):
+    url = 'https://api.groupme.com/v3/bots/post'
     
-        data = {
-            'bot_id' : os.getenv('GROUPME_BOT_ID'),
-            'text'   : msg,
-        }
+    data = {
+        'bot_id' : os.getenv('GROUPME_BOT_ID'),
+        'text'   : msg,
+    }
     
-        request = Request(url, urlencode(data).encode())
-        json = urlopen(request).read().decode()
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
 
-def send_dadjoke(isTimeout):
-    if not isTimeout:
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-           'Accept': 'text/plain',
-           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-           'Accept-Encoding': 'none',
-           'Accept-Language': 'en-US,en;q=0.8',
-           'Connection': 'keep-alive'}
+def send_dadjoke():
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/plain',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+    
+    print('In send dadjoke!')
+    request = Request('https://icanhazdadjoke.com/', headers=headers)
+    json = urlopen(request).read().decode()
+    
+    url = 'https://api.groupme.com/v3/bots/post'
+    
+    data = {
+        'bot_id' : os.getenv('GROUPME_BOT_ID'),
+        'text'   : json.replace('\\n', ' ').replace('"', '').replace('\\t', '    ').replace('\\', '"'),
+    }
+    
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
 
-        request = Request('https://icanhazdadjoke.com/', headers=headers)
-        json = urlopen(request).read().decode()
-        
-        url = 'https://api.groupme.com/v3/bots/post'
+def send_fortune():
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+    
+    print('In send fortune!')
+    
+    request = Request('https://helloacm.com/api/fortune/', headers=headers)
+    json = urlopen(request).read().decode()
+    
+    url = 'https://api.groupme.com/v3/bots/post'
+    
+    data = {
+        'bot_id' : os.getenv('GROUPME_BOT_ID'),
+        'text'   : json.replace('\\n', ' ').replace('"', '').replace('\\t', '    ').replace('\\', '"'),
+    }
 
-        data = {
-            'bot_id' : os.getenv('GROUPME_BOT_ID'),
-            'text'   : json.replace('\\n', ' ').replace('"', '').replace('\\t', '    ').replace('\\', '"'),
-        }
-
-        request = Request(url, urlencode(data).encode())
-        json = urlopen(request).read().decode()
-
-def send_fortune(isTimeout):
-
-    if not isTimeout:
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-           'Accept-Encoding': 'none',
-           'Accept-Language': 'en-US,en;q=0.8',
-           'Connection': 'keep-alive'}
-
-        request = Request('https://helloacm.com/api/fortune/', headers=headers)
-        json = urlopen(request).read().decode()
-        
-        url = 'https://api.groupme.com/v3/bots/post'
-
-        data = {
-            'bot_id' : os.getenv('GROUPME_BOT_ID'),
-            'text'   : json.replace('\\n', ' ').replace('"', '').replace('\\t', '    ').replace('\\', '"'),
-        }
-
-        request = Request(url, urlencode(data).encode())
-        json = urlopen(request).read().decode()
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
 
